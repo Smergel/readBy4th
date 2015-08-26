64b22c12b907b35651d84aa604560fa8b543ed70
Jacob Smergel <jacob.smergel@gmail.com>
2015-08-26 16:08:40 -0400

    Fixed #1 linked the databases with has one...

--- db/schema.rb
+++ db/schema.rb
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(version: 20150826012425) do
 
   create_table "posts", force: :cascade do |t|
     t.integer  "user_id"
-    t.integer  "program_id"
+    t.integer  "partner_id"
     t.integer  "post_type"
     t.string   "post_description"
     t.string   "post_attachment_file_name"
