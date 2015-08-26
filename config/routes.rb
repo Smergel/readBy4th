535ea6b1b340a3d17acb8058c6e0b1c4eb0fdc4b
Jacob Smergel <jacob.smergel@gmail.com>
2015-08-26 16:46:15 -0400

    Fixed #2 home and partner pages linked

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   resources :participants
   resources :partners
   resources :profiles
-  resources :users
+  # resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
