03bf6ee53878df4c33e721b0731b7afee4245fdb
William Bolton <william.ellet@gmail.com>
2015-09-01 11:04:25 -0700

    commit before authentication branch

--- db/schema.rb
+++ db/schema.rb
@@ -81,7 +81,7 @@ ActiveRecord::Schema.define(version: 20150826212954) do
 
   create_table "posts", force: :cascade do |t|
     t.integer  "user_id"
-    t.integer  "partner_id"
+    t.integer  "program_id"
     t.integer  "post_type"
     t.string   "post_description"
     t.string   "post_attachment_file_name"
