d46d76d3002ee8525c595d145435cc29fef17429
William Bolton <william.ellet@gmail.com>
2015-09-09 16:09:01 -0400

    added controller method for handling user info not permitted by devise--found in applications controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,13 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    if params[:approved] == "false"
-      @users = User.find_all_by_approved(false)
-    else
-      @users = User.all
-    end
+
+    # Approval methods -- not planning to have it working for demo.
+    # if params[:approved] == "false"
+    #   @users = User.find_all_by_approved(false)
+    # else
+    #   @users = User.all
+    # end
   end
 
   # GET /users/1
@@ -27,19 +29,19 @@ class UsersController < ApplicationController
 
   # POST /users
   # POST /users.json
-  def create
-    @user = User.new(user_params)
-    puts @user.inspect
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # def create
+  #   @user = User.new(user_params)
+  #   puts @user.inspect
+  #   respond_to do |format|
+  #     if @user.save
+  #       format.html { redirect_to @user, notice: 'User was successfully created.' }
+  #       format.json { render :show, status: :created, location: @user }
+  #     else
+  #       format.html { render :new }
+  #       format.json { render json: @user.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,16 +10,16 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  def active_for_authentication?
-    super && approved?
-  end
+  # def active_for_authentication?
+  #   super && approved?
+  # end
 
-  def inactive_message
-    if !approved?
-      :not_approved
-    else
-      super
-    end
-  end
+  # def inactive_message
+  #   if !approved?
+  #     :not_approved
+  #   else
+  #     super
+  #   end
+  # end
 
 end
