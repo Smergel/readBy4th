1ccebd9cc60fbf42ae0381cfe57ef649181122bd
devalresourse <adeola.olanrewaju@gmail.com>
2015-12-11 14:43:04 -0500

    Heading and footer

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20151209150302) do
-
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
